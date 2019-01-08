f95df8e79e67d189b5d7d5965af15944f2739174
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2019-01-07 20:51:16 -0500

    change package name to 'WhatsApp'

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+PLATFORM="linux"
+ARCH="x64"
+
 nativefier --zoom 1.33 --name whatsapp --inject whatsapp-fix.js https://web.whatsapp.com/
+
+# Change package name from 'whatsapp-nativefier-XXXXXX' to 'WhatsApp'
+sed -i -E 's/whatsapp-nativefier-.{6}/WhatsApp/' whatsapp-${PLATFORM}-${ARCH}/resources/app/package.json
