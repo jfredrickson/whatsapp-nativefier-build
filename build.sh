593362aafd80e3d1b629344c9385afc1c1840d92
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2019-01-08 23:41:31 -0500

    reduce zoom from 133% to 110%

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 PLATFORM="linux"
 ARCH="x64"
 
-nativefier --zoom 1.33 --name whatsapp --inject whatsapp-fix.js https://web.whatsapp.com/
+nativefier --zoom 1.1 --name whatsapp --inject whatsapp-fix.js https://web.whatsapp.com/
 
 # Change package name from 'whatsapp-nativefier-XXXXXX' to 'WhatsApp'
 sed -i -E 's/whatsapp-nativefier-.{6}/WhatsApp/' whatsapp-${PLATFORM}-${ARCH}/resources/app/package.json
