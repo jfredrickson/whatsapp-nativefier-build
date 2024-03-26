7c979b78554289896bee9fc9926acf56e2ba101e
Jeff Fredrickson <jeff@ejeff.org>
2024-03-26 15:13:28 -0600

    fix permissions

--- build.sh
+++ build.sh
@@ -2,8 +2,14 @@
 
 PLATFORM="linux"
 ARCH="x64"
+NAME="WhatsApp"
+DIR="${NAME}-${PLATFORM}-${ARCH}"
 
-nativefier --zoom 1.1 --name whatsapp --inject whatsapp-fix.js https://web.whatsapp.com/
+nativefier https://web.whatsapp.com/ --zoom 1.1 --name $NAME --inject whatsapp-fix.js
 
 # Change package name from 'whatsapp-nativefier-XXXXXX' to 'WhatsApp'
-sed -i -E 's/whatsapp-nativefier-.{6}/WhatsApp/' whatsapp-${PLATFORM}-${ARCH}/resources/app/package.json
+sed -i -E 's/whatsapp-nativefier-.{6}/$NAME/' ${DIR}/resources/app/package.json
+
+# Fix permissions
+find ${DIR} -type f -exec chmod go+r {} \;
+find ${DIR} -type d -exec chmod go+rx {} \;
